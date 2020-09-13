af356787172df531be75ddde5d23bc3c8996ae70
Priya Mondal <62085086+Priya96-sketch@users.noreply.github.com>
2020-09-13 12:58:55 +0530

    Create Preprocessor_directives_uses.cpp

---
+++ Practice/Preprocessor_directives_uses.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+//Not using "namespace standard" for avoiding the bad practice.
+#define gfg 2020  //"define" is a directive used to define a macro and doesn't need a ";" to end.
+
+//Applying conditional compilation:
+#if gfg>200
+   #undef gfg  //used to undefine a directive
+   #define gfg 1000  
+#elif gfg<50
+   #undef gfg
+   #define gfg 50
+#else
+    #undef gfg
+    #define gfg 100
+#endif
+
+int main()
+{
+    std::cout<<"Value is now: "<<gfg;  //using scope resolution operators after "standard" instead of "namespace standard"
+    return 0;
+}
